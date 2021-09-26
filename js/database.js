080faf457e8b715b4315f783c9fd9ce24622f522
Etchavious <34903080+Etchavious@users.noreply.github.com>
2021-09-26 01:07:25 -0500

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,16 +1,22 @@
 //Build Tabulator
 var table = new Tabulator("#example-table", {
-    height:"311px",
-    layout:"fitColumns",
-    ajaxURL:"data/itemdatabase.json",
-	ajaxConfig:"GET",
-	ajaxContentType:"json",
-    ajaxProgressiveLoad:"scroll",
-    paginationSize:20,
+    ajaxConfig:{
+		method: "GET",
+		mode: "cors"
+	},
+	ajaxURL:"https://etchavious.github.io/KMS2AH/data/itemdatabase.json",
+	layout:"fitData",
     placeholder:"No Data Set",
     columns:[
         {title:"ID", field:"ID", sorter:"string"},
-        {title:"Name - Korean", field:"NAME_KR", sorter:"string"},
-        {title:"Name - English", field:"NAME_EN", sorter:"string"},
+        {title:"Name - Korean", field:"Name_KR"},
+        {title:"Name - English", field:"Name_EN"},
     ],
+	ajaxResponse:function(url, params, response){
+        //url - the URL of the request
+        //params - the parameters passed with the request
+        //response - the JSON object returned in the body of the response.
+
+        return response.data; //pass the data array into Tabulator
+    },
 });
